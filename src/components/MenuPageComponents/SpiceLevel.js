4330376a749115628ee2cde0caba9cb015b040a5
Veronica Adler <vlynnadler@gmail.com>
2021-07-26 17:57:44 -0400

    added functioning tabs and menu to menupage

---
+++ src/components/MenuPageComponents/SpiceLevel.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function SpiceLevel() {
+    return(
+        <div class="row">
+        <div class="col">
+            <i class="fa fa-star"></i> Mild
+            <i class="fa fa-star"></i><i class="fa fa-star"></i>Spicy
+            <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>Hot
+            and Spicy
+        </div>
+    </div>
+    )
+}
+
+export default SpiceLevel
